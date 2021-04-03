bd6e6efdcf945771f769f20553598dc2287c8fa7
YeopJae-Mon <1015samuel@naver.com>
2021-04-03 14:04:46 +0900

    2021-04-03 Make Result Activity

--- app/src/main/java/com/example/practice_moveball/ClearActivity.java
+++ app/src/main/java/com/example/practice_moveball/ClearActivity.java
@@ -13,7 +13,7 @@ public class ClearActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clear);
 
-        Button button1 = (Button) findViewById(R.id.btn05);
+        Button button1 = (Button) findViewById(R.id.btn);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +22,19 @@ public class ClearActivity extends Activity{
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
--- app/src/main/java/com/example/practice_moveball/FailActivity.java
+++ app/src/main/java/com/example/practice_moveball/FailActivity.java
@@ -1,4 +1,40 @@
 package com.example.practice_moveball;
 
-public class FailActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clear);
+
+        Button button1 = (Button) findViewById(R.id.btn);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
